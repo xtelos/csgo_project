dd6b4541f538e5e2e3fb8b70ac448ddfe4c19ad7
dylanfodor <fodord@moravian.edu>
2021-11-05 11:24:42 -0400

    added python file for database creation

--- create_db.py
+++ create_db.py
@@ -1,7 +1,35 @@
+import mysql.connector
+import dotenv
+import os
+
+
+def connectToMysql():
+    dotenv.load_dotenv()
+
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+
+    conn = mysql.connector.connect(username=username, password=password, host='localhost')
+    cursor = conn.cursor()
+
+    return conn, cursor
+
 
 def create():
-    pass
+    cursor.execute('CREATE DATABASE IF NOT EXISTS csgo;')
+    cursor.execute('USE csgo;')
+
+    cursor.execute('DROP TABLE IF EXISTS players;')
+    cursor.execute('DROP TABLE IF EXISTS matches;')
+    cursor.execute('DROP TABLE IF EXISTS maps;')
+    cursor.execute('DROP TABLE IF EXISTS playerAnalytics;')
+
+    cursor.execute('CREATE TABLE players (name VARCHAR(255), team VARCHAR(255), country VARCHAR(255));')
+    cursor.execute('CREATE TABLE matches (date DATE, matchID INT, eventName VARCHAR(255), team1 VARCHAR(255), team2 VARCHAR(255), bestOf INT, winner VARCHAR(255));')
+    cursor.execute('CREATE TABLE maps (mapName VARCHAR(255), pickRate FLOAT, banRate FLOAT);')
+    cursor.execute('CREATE TABLE playerAnalytics (playerName VARCHAR(255), teamName VARCHAR(255), matchID INT, kills INT, deaths INT);')
 
 
 if __name__ == "__main__":
+    conn, cursor = connectToMysql()
     create()
